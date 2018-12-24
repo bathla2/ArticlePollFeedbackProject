991a09428155649bd110d39ce99b2cf4ace662e1
study <bathlaa15@gmail.com>
2018-12-23 21:23:57 -0500

    new commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var cheerio = require("cheerio");
 var db = require("./models");
 var path = require("path");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
